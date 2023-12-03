93cc35683e1e574739b9aa6aa5e618ee0724eb81
maciejpiasecki <wrx75789@student.wsb.wroclaw.pl>
2023-12-03 23:21:43 +0100

    Dodanie komentarzy

--- login.js
+++ login.js
@@ -1,16 +1,28 @@
+// Wyciągnięcie z dokumentu elementów na podstawie ich ID i przypisanie ich do stałych
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+
+
+// Dodanie do przycisku login funkcji która będzie uruchamiać się podczas kliknięcia na niego
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+    // Wyciągnięcie z inputów username i password wprowadzonych wartości
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-
-    if (username === "" && password === "") {
+    // Walidacja wprowadzonych danych
+    if (username === "Student" && password === "1234") {
+        //Komunikat o zalogowaniu się
         alert("You have successfully logged in.");
+        // Odświeżenie strony
         location.reload();
     } else {
+
         loginErrorMsg.style.opacity = 1;
+
+
     }
-})\\\\
+
+})
